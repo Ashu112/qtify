fe05029646a23dd0269b95a6e8be994943a4a259
Ashu112 <kashu2611@gmail.com>
2023-10-18 19:18:10 +0530

    Card Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import { fetchTopAlbums } from "./api/api";
+import Card from "./components/Card/Card";
 
 function App() {
+  const [topAlbumsData, setTopAlbumsData] = useState([]);
+
+  const generateTopAlbumsData = async () => {
+    try {
+      const data = await fetchTopAlbums();
+      setTopAlbumsData(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    generateTopAlbumsData();
+  }, []);
+
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      {topAlbumsData.map((item) => {
+        return <Card data={item} type="album" key={item.id} />;
+      })}
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./Card.module.css";
+import Chip from '@mui/material/Chip';
+import Tooltip from '@mui/material/Tooltip';
+
+
+const Card = ({ data, type }) => {
+  const getCard = (type) => {
+    switch (type) {
+      case "album": {
+        const { image, follows, title, songs } = data;
+        return (
+            <Tooltip title={`${songs.length} songs`} placement="top" arrow>
+          <div className={styles.wrapper}>
+            <div className={styles.card}>
+              <img src={image} alt="album" />
+              <div className={styles.banner}>
+                <Chip label={`${follows} Follows`} size='small' className={styles.chip}/>
+              </div>
+            </div>
+            <div className={styles.titleWrapper}>
+              <p>{title}</p>
+            </div>
+          </div>
+          </Tooltip>
+        );
+      }
+      default:
+        return <></>;
+    }
+  };
+
+  return getCard(type);
+};
+
+export default Card;
